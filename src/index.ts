fe057e82ab8a2e7feedcc1c30aca272b739a04ec
Jean-Christophe Baey <jean-christophe.baey@renault.com>
2023-04-05 16:55:16 +0200

    Add dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
+/* eslint-disable no-console */
+import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config();
+
 export async function main(): Promise<void> {
-  // eslint-disable-next-line no-console
   console.log("=== Starting ===");
 }
 
 (async () => {
-  // eslint-disable-next-line no-console
   main().catch((error) => console.error(error));
 })();
